770c8879a258b3f947e4ea3254b220e8351995f3
sharmila-R1 <sharmilaramu772@gmail.com>
2026-08-04 15:27:18 +0530

    Add distance attribute to Route class

--- src/main/java/model/Route.java
+++ src/main/java/model/Route.java
@@ -1,43 +1,81 @@
+[3:12 PM] Madhumitha M
 package model;
-
+ 
 import java.util.List;
-
+ 
 public class Route {
-
+ 
     private String routeId;
+
     private String routeName;
+
     private List<String> stopIds;
 
+    private double distance;
+ 
     public Route() {
-    }
 
-    public Route(String routeId, String routeName, List<String> stopIds) {
+    }
+ 
+    public Route(String routeId, String routeName, List<String> stopIds, double distance) {
+
         this.routeId = routeId;
-        this.routeName = routeName;
-        this.stopIds = stopIds;
-    }
 
+        this.routeName = routeName;
+
+        this.stopIds = stopIds;
+
+        this.distance = distance;
+
+    }
+ 
     public String getRouteId() {
+
         return routeId;
-    }
 
+    }
+ 
     public void setRouteId(String routeId) {
+
         this.routeId = routeId;
-    }
 
+    }
+ 
     public String getRouteName() {
+
         return routeName;
-    }
 
+    }
+ 
     public void setRouteName(String routeName) {
+
         this.routeName = routeName;
-    }
 
+    }
+ 
     public List<String> getStopIds() {
+
         return stopIds;
+
+    }
+ 
+    public void setStopIds(List<String> stopIds) {
+
+        this.stopIds = stopIds;
+
+    }
+ 
+    public double getDistance() {
+
+        return distance;
+
+    }
+ 
+    public void setDistance(double distance) {
+
+        this.distance = distance;
+
     }
 
-    public void setStopIds(List<String> stopIds) {
-        this.stopIds = stopIds;
-    }
 }
+ 
