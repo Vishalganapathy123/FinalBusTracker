9d85cffec7d5d463dfd0dd8d984ba60bd9faa6df
sharmila-R1 <sharmilaramu772@gmail.com>
2026-08-05 13:39:18 +0530

    Update BusLocationRepository.java

--- src/main/java/repository/BusLocationRepository.java
+++ src/main/java/repository/BusLocationRepository.java
@@ -16,49 +16,34 @@ public class BusLocationRepository {
     private MongoCollection<BusLocation> collection;
 
     public BusLocationRepository() {
-
         MongoDatabase database = MongoDBConfig.getDatabase();
         collection = database.getCollection("BusLocation", BusLocation.class);
-
     }
 
     // Add Bus Location
     public void addBusLocation(BusLocation busLocation) {
-
         collection.insertOne(busLocation);
-
     }
 
     // Get All Bus Locations
     public List<BusLocation> getAllBusLocations() {
-
         List<BusLocation> locationList = new ArrayList<>();
-
         collection.find().into(locationList);
-
         return locationList;
-
     }
 
     // Get Bus Location By Bus Id
     public BusLocation getBusLocationByBusId(String busId) {
-
         return collection.find(eq("busId", busId)).first();
-
     }
 
     // Update Bus Location
     public void updateBusLocation(BusLocation busLocation) {
-
         collection.replaceOne(eq("busId", busLocation.getBusId()), busLocation);
-
     }
 
     // Delete Bus Location
     public void deleteBusLocation(String busId) {
-
         collection.deleteOne(eq("busId", busId));
-
     }
-
 }
