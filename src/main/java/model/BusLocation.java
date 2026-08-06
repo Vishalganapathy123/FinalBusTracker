6b12989413acba3c68252712dc4d20f5f44ae8db
sharmila-R1 <sharmilaramu772@gmail.com>
2026-08-06 10:34:34 +0530

    Update BusLocation.java

--- src/main/java/model/BusLocation.java
+++ src/main/java/model/BusLocation.java
@@ -1,90 +1,36 @@
 package model;
 
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+
 public class BusLocation {
 
+    @BsonId
+    @BsonProperty("busId")
     private String busId;
+
+    @BsonProperty("latitude")
     private double latitude;
+
+    @BsonProperty("longitude")
     private double longitude;
+
+    @BsonProperty("speed")
     private double speed;
 
+    @BsonProperty("currentStop")
     private String currentStop;
+
+    @BsonProperty("nextStop")
     private String nextStop;
 
+    @BsonProperty("lastUpdated")
     private String lastUpdated;
 
     public BusLocation() {
     }
 
-    public BusLocation(String busId,
-                       double latitude,
-                       double longitude,
-                       double speed,
-                       String currentStop,
-                       String nextStop,
-                       String lastUpdated) {
+    // Constructors
 
-        this.busId = busId;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.speed = speed;
-        this.currentStop = currentStop;
-        this.nextStop = nextStop;
-        this.lastUpdated = lastUpdated;
-    }
-
-    public String getBusId() {
-        return busId;
-    }
-
-    public void setBusId(String busId) {
-        this.busId = busId;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public double getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(double speed) {
-        this.speed = speed;
-    }
-
-    public String getCurrentStop() {
-        return currentStop;
-    }
-
-    public void setCurrentStop(String currentStop) {
-        this.currentStop = currentStop;
-    }
-
-    public String getNextStop() {
-        return nextStop;
-    }
-
-    public void setNextStop(String nextStop) {
-        this.nextStop = nextStop;
-    }
-
-    public String getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public void setLastUpdated(String lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
+    // Getters and Setters
 }
