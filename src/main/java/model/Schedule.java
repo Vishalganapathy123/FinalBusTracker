363920a0f6fa83e16c23ecf2c7a54c74a36ed47f
sharmila-R1 <sharmilaramu772@gmail.com>
2026-08-06 10:35:11 +0530

    Update Schedule.java

--- src/main/java/model/Schedule.java
+++ src/main/java/model/Schedule.java
@@ -2,25 +2,53 @@ package model;
 
 import java.util.List;
 
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+
 public class Schedule {
 
+    @BsonId
+    @BsonProperty("scheduleId")
     private String scheduleId;
+
+    @BsonProperty("busId")
     private String busId;
+
+    @BsonProperty("routeId")
     private String routeId;
+
+    @BsonProperty("sourceName")
+    private String sourceName;
+
+    @BsonProperty("destinationName")
+    private String destinationName;
+
+    @BsonProperty("departureTime")
     private String departureTime;
+
+    @BsonProperty("arrivalTime")
     private String arrivalTime;
+
+    @BsonProperty("operatingDays")
     private List<String> operatingDays;
 
     public Schedule() {
     }
 
-    public Schedule(String scheduleId, String busId, String routeId,
-                    String departureTime, String arrivalTime,
+    public Schedule(String scheduleId,
+                    String busId,
+                    String routeId,
+                    String sourceName,
+                    String destinationName,
+                    String departureTime,
+                    String arrivalTime,
                     List<String> operatingDays) {
 
         this.scheduleId = scheduleId;
         this.busId = busId;
         this.routeId = routeId;
+        this.sourceName = sourceName;
+        this.destinationName = destinationName;
         this.departureTime = departureTime;
         this.arrivalTime = arrivalTime;
         this.operatingDays = operatingDays;
@@ -48,6 +76,22 @@ public class Schedule {
 
     public void setRouteId(String routeId) {
         this.routeId = routeId;
+    }
+
+    public String getSourceName() {
+        return sourceName;
+    }
+
+    public void setSourceName(String sourceName) {
+        this.sourceName = sourceName;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
     }
 
     public String getDepartureTime() {
