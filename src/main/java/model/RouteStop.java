14fe4d13257052b87be3ddd52cdd51caf7be5301
sharmila-R1 <sharmilaramu772@gmail.com>
2026-08-06 10:33:32 +0530

    Update RouteStop.java

--- src/main/java/model/RouteStop.java
+++ src/main/java/model/RouteStop.java
@@ -1,17 +1,28 @@
 package model;
 
+import org.bson.codecs.pojo.annotations.BsonProperty;
+
 public class RouteStop {
 
+    @BsonProperty("stopId")
     private String stopId;
+
+    @BsonProperty("stopName")
     private String stopName;
+
+    @BsonProperty("stopOrder")
     private int stopOrder;
+
+    @BsonProperty("distanceFromPrevious")
     private double distanceFromPrevious;
 
     public RouteStop() {
     }
 
-    public RouteStop(String stopId, String stopName,
-                     int stopOrder, double distanceFromPrevious) {
+    public RouteStop(String stopId,
+                     String stopName,
+                     int stopOrder,
+                     double distanceFromPrevious) {
 
         this.stopId = stopId;
         this.stopName = stopName;
@@ -19,35 +30,5 @@ public class RouteStop {
         this.distanceFromPrevious = distanceFromPrevious;
     }
 
-    public String getStopId() {
-        return stopId;
-    }
-
-    public void setStopId(String stopId) {
-        this.stopId = stopId;
-    }
-
-    public String getStopName() {
-        return stopName;
-    }
-
-    public void setStopName(String stopName) {
-        this.stopName = stopName;
-    }
-
-    public int getStopOrder() {
-        return stopOrder;
-    }
-
-    public void setStopOrder(int stopOrder) {
-        this.stopOrder = stopOrder;
-    }
-
-    public double getDistanceFromPrevious() {
-        return distanceFromPrevious;
-    }
-
-    public void setDistanceFromPrevious(double distanceFromPrevious) {
-        this.distanceFromPrevious = distanceFromPrevious;
-    }
+    // Getters and Setters
 }
